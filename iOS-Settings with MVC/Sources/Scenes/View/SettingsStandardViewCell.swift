762cd7362e4e8e3c2e28e450d9cabb36db92259a
Anastasiia Maksimova <sia.m94@ya.ru>
2021-08-19 23:44:34 +0300

    Add SettingsLabelTableViewCell().

---
+++ iOS-Settings with MVC/Sources/Scenes/View/SettingsStandardViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsStandardViewCell.swift
+//  iOS-Settings with MVC
+//
+//  Created by Анастасия on 19.08.2021.
+//
+
+import Foundation
---
+++ iOS-Settings with MVC/Sources/Scenes/View/SettingsSwitchTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsSwitchTableViewCell.swift
+//  iOS-Settings with MVC
+//
+//  Created by Анастасия on 19.08.2021.
+//
+
+import Foundation
